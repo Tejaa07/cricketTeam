35ee6d3da0b1ecc3ab745240c3751d664fbfcd7e
Tejaa07 <saitejakubbaji@gmail.com>
2024-06-02 17:05:57 +0000

    CricketTeam

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const path = require('path')
 
 const {open} = require('sqlite')
 const sqlite3 = require('sqlite3')
+
 const app = express()
 app.use(express.json())
+
 const dbPath = path.join(__dirname, 'cricketTeam.db')
 
 let db = null
@@ -42,7 +44,7 @@ app.get('/players/', async (request, response) => {
 //API2
 app.post('/players/', async (request, response) => {
   const playerDetails = request.body
-  const {player_id, player_name, jersey_number, role} = playerDetails
+  const {player_name, jersey_number, role} = playerDetails
 
   const addPlayer = `
   INSERT INTO
@@ -58,3 +60,23 @@ app.post('/players/', async (request, response) => {
 })
 
 //API 3
+app.put('/players/:playerId', async (request, response) => {
+  const {playerId} = request.params
+  const getPlayersQuery = `
+  select
+  *
+  from
+  cricket_team
+  where
+  player_id = ${playerId};`
+
+  const player = await db.get(getPlayerQuery)
+  const {player_id, player_name, jersey_number, role} = player
+  const dbResponse = {
+    playerId: player_id,
+    playerName: player_name,
+    jerseyNumber: jersey_number,
+    role: role,
+  }
+  response.send(dbResponse)
+})
